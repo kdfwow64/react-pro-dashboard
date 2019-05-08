a297cd08ef1063a59be7778c5768e72d1a2e2820
Rishabh Tayal <rtayal11@gmail.com>
2019-05-08 13:51:17 -0500

    Update index.js

--- architectui-react-pro/src/DemoPages/Dashboards/PreviewApp/index.js
+++ architectui-react-pro/src/DemoPages/Dashboards/PreviewApp/index.js
@@ -1,10 +1,25 @@
 import React, { Fragment } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import PageTitle from '../../../Layout/AppMain/PageTitle';
 
 export default class PreviewApp extends React.Component {
 
     render() {
         return (
             <Fragment>
+                <ReactCSSTransitionGroup
+                    component="div"
+                    transitionName="TabsAnimation"
+                    transitionAppear={true}
+                    transitionAppearTimeout={0}
+                    transitionEnter={false}
+                    transitionLeave={false}>
+                    <PageTitle
+                        heading="Preview Your App"
+                        subheading="This is an example dashboard created using build-in elements and components."
+                        icon="pe-7s-car icon-gradient bg-mean-fruit"
+                    />
+                </ReactCSSTransitionGroup>
             </Fragment>
         );
     }
