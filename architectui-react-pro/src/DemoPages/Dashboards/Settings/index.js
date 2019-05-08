dabfb564a221e78d01a4dec912f1f5acd679c38b
Rishabh Tayal <rtayal11@gmail.com>
2019-05-08 14:17:29 -0500

    settings page

--- architectui-react-pro/src/DemoPages/Dashboards/Settings/index.js
+++ architectui-react-pro/src/DemoPages/Dashboards/Settings/index.js
@@ -1,10 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import { Card, CardBody, CardTitle, Input } from 'reactstrap';
 
 export default class SettingsDashboard extends Component {
 
     render() {
         return (
-            <div>Hello world</div>
+            <Fragment>
+                <ReactCSSTransitionGroup
+                    component="div"
+                    transitionName="TabsAnimation"
+                    transitionAppear={true}
+                    transitionAppearTimeout={0}
+                    transitionEnter={false}
+                    transitionLeave={false}>
+                    <Card className="main-card mb-3">
+                        <CardBody>
+                            <CardTitle>Storefront Token</CardTitle>
+                            <p>Your storefront token from Private app. See how to create one.</p>
+                            <Input type="text"/>
+                        </CardBody>
+                    </Card>
+                </ReactCSSTransitionGroup>
+            </Fragment>
         )
     }
 }
