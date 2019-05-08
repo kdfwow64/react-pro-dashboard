41da76cc52b3f989849ac6a0cba2378021546396
Rishabh Tayal <rtayal11@gmail.com>
2019-05-07 23:50:50 -0500

    Update ScheduledNotifications.js

--- architectui-react-pro/src/DemoPages/Dashboards/PushNotifications/ScheduledNotifications.js
+++ architectui-react-pro/src/DemoPages/Dashboards/PushNotifications/ScheduledNotifications.js
@@ -1,11 +1,58 @@
 import React, { Fragment } from 'react';
+import { Card, CardBody, CardTitle, Table } from 'reactstrap';
 
 export default class ScheduledNotifications extends React.Component {
 
     render() {
         return (
             <Fragment>
-                scheduled notifications
+                <Card className="main-card mb-3">
+                    <CardBody>
+                        <CardTitle>Table responsive</CardTitle>         
+                            <Table responsive className="mb-0">
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Table heading</th>
+                                    <th>Table heading</th>
+                                    <th>Table heading</th>
+                                    <th>Table heading</th>
+                                    <th>Table heading</th>
+                                    <th>Table heading</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                    <td>Table cell</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </CardBody>
+                </Card>
             </Fragment>
         );
     }
