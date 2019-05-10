0ecde12b51691b89e00cc1a9c8e5b7e48c865ac1
Rishabh Tayal <rtayal11@gmail.com>
2019-05-10 15:48:28 -0500

    tutorials

--- architectui-react-pro/src/DemoPages/Dashboards/Tutorials/index.js
+++ architectui-react-pro/src/DemoPages/Dashboards/Tutorials/index.js
@@ -1,26 +1,116 @@
-import React, { Component, Fragment } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
-import PageTitle from '../../../Layout/AppMain/PageTitle';
+import React, { Component, Fragment } from "react";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import { Button, Card, Col, Modal, ModalBody, Row } from "reactstrap";
+import avatar3 from "../../../assets/utils/images/avatars/4.jpg";
+import avatar5 from "../../../assets/utils/images/avatars/5.jpg";
+import bg1 from "../../../assets/utils/images/dropdown-header/abstract1.jpg";
+import bg2 from "../../../assets/utils/images/dropdown-header/city2.jpg";
+import PageTitle from "../../../Layout/AppMain/PageTitle";
 
 export default class Tutorials extends Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
 
-    render() {
-        return (
-            <Fragment>
-                <ReactCSSTransitionGroup
-                    component="div"
-                    transitionName="TabsAnimation"
-                    transitionAppear={true}
-                    transitionAppearTimeout={0}
-                    transitionEnter={false}
-                    transitionLeave={false}>
-                    <PageTitle
-                        heading="Tutorials"
-                        subheading="Example of a Dashboard page built with ArchitectUI."
-                        icon="pe-7s-video icon-gradient bg-sunny-morning"
-                    />
-                </ReactCSSTransitionGroup>
-            </Fragment>
-        )
-    }
+    this.state = {
+      modal: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+  render() {
+    return (
+      <Fragment>
+        <ReactCSSTransitionGroup
+          component="div"
+          transitionName="TabsAnimation"
+          transitionAppear={true}
+          transitionAppearTimeout={0}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <PageTitle
+            heading="Tutorials"
+            subheading="Example of a Dashboard page built with ArchitectUI."
+            icon="pe-7s-video icon-gradient bg-sunny-morning"
+          />
+          <div>
+            <Row>
+              <Col
+                md="6"
+                lg="4"
+                onClick={() => {
+                  this.setState({ modal: true });
+                }}
+              >
+                <Card className="mb-3 profile-block">
+                  <div className="dropdown-menu-header">
+                    <img src={bg1} className="profile-blur opacity-10" />
+                    <div className="profile-inner bg-warning opacity-5" />
+                    <div className="menu-header-content">
+                      <div className="avatar-icon-wrapper avatar-icon-lg">
+                        <div className="font-icon-wrapper font-icon-lg">
+                          <i className="pe-7s-play" alt="Avatar 5" />
+                        </div>
+                      </div>
+                      <h5 className="menu-header-title">AppIt Walkthrough</h5>
+                    </div>
+                  </div>
+                </Card>
+              </Col>
+              <Col
+                md="6"
+                lg="4"
+                onClick={() => {
+                  this.setState({ modal: true });
+                }}
+              >
+                <Card className="mb-3 profile-block">
+                  <div className="dropdown-menu-header">
+                    <img src={bg2} className="profile-blur opacity-10" />
+                    <div className="profile-inner bg-dark opacity-6" />
+                    <div className="menu-header-content">
+                      <div className="avatar-icon-wrapper avatar-icon-lg">
+                        <div className="font-icon-wrapper font-icon-lg">
+                          <i className="pe-7s-play" alt="Avatar 5" />
+                        </div>
+                      </div>
+                      <h5 className="menu-header-title">AppIt Explainer</h5>
+                    </div>
+                  </div>
+                </Card>
+              </Col>
+            </Row>
+            <Modal
+              isOpen={this.state.modal}
+              toggle={this.toggle}
+              className={this.props.className}
+              width={560}
+            >
+              {/* <ModalHeader toggle={this.toggle}>Modal title</ModalHeader> */}
+              <ModalBody>
+                <iframe
+                  width="560"
+                  height="315"
+                  src="https://www.youtube.com/embed/gvicwQDqkIU"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                />
+              </ModalBody>
+              {/* <ModalFooter>
+                <Button color="link" onClick={this.toggle}>
+                  Ok
+                </Button>
+              </ModalFooter> */}
+            </Modal>
+          </div>
+        </ReactCSSTransitionGroup>
+      </Fragment>
+    );
+  }
 }
