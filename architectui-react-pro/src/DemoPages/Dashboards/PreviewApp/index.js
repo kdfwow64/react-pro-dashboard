dd495e8da44f5321d396c3efbb1f4ff9819d0e60
Rishabh Tayal <rtayal11@gmail.com>
2019-05-12 14:28:20 -0500

    Update index.js

--- architectui-react-pro/src/DemoPages/Dashboards/PreviewApp/index.js
+++ architectui-react-pro/src/DemoPages/Dashboards/PreviewApp/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import PageTitle from '../../../Layout/AppMain/PageTitle';
+import { Button } from 'reactstrap';
 
 export default class PreviewApp extends React.Component {
 
@@ -19,6 +20,9 @@ export default class PreviewApp extends React.Component {
                         subheading="This is an example dashboard created using build-in elements and components."
                         icon="pe-7s-phone icon-gradient bg-mean-fruit"
                     />
+                    <p>Your app is ready to preview on your device.</p>
+                    <p>Download the AppIt preview app on your device.</p>
+                    <Button>Download</Button>
                 </ReactCSSTransitionGroup>
             </Fragment>
         );
