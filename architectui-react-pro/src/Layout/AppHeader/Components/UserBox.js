e478e5365fb34e81811563bb3176e185ab3dd42b
Rishabh Tayal <rtayal11@gmail.com>
2019-05-13 16:55:06 -0500

    Update UserBox.js

--- architectui-react-pro/src/Layout/AppHeader/Components/UserBox.js
+++ architectui-react-pro/src/Layout/AppHeader/Components/UserBox.js
@@ -1,58 +1,35 @@
-import React, {Fragment} from 'react';
-
-import Ionicon from 'react-ionicons';
-
-import PerfectScrollbar from 'react-perfect-scrollbar';
-
-import {
-    DropdownToggle, DropdownMenu,
-    Nav, Col, Row, Button, NavItem, NavLink,
-    UncontrolledTooltip, UncontrolledButtonDropdown
-} from 'reactstrap';
-
-import {
-    toast,
-    Bounce
-} from 'react-toastify';
-
-
-import {
-    faAngleDown,
-
-} from '@fortawesome/free-solid-svg-icons';
-
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-
-import city3 from '../../../assets/utils/images/dropdown-header/city3.jpg';
-import avatar1 from '../../../assets/utils/images/avatars/1.jpg';
-import { getShop } from '../../../utilities/shop-utils';
+import React, { Fragment } from "react";
+import { Bounce, toast } from "react-toastify";
+import { Button } from "reactstrap";
+import { getShop } from "../../../utilities/shop-utils";
 
 class UserBox extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            active: false,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false
+    };
+  }
 
-    }
-
-    notify2 = () => this.toastId = toast("You don't have any new items in your calendar for today! Go out and play!", {
+  notify2 = () =>
+    (this.toastId = toast(
+      "You don't have any new items in your calendar for today! Go out and play!",
+      {
         transition: Bounce,
         closeButton: true,
         autoClose: 5000,
-        position: 'bottom-center',
-        type: 'success'
-    });
+        position: "bottom-center",
+        type: "success"
+      }
+    ));
 
-
-    render() {
-
-        return (
-            <Fragment>
-                <div className="header-btn-lg pr-0">
-                    <div className="widget-content p-0">
-                        <div className="widget-content-wrapper">
-                            {/* <div className="widget-content-left">
+  render() {
+    return (
+      <Fragment>
+        <div className="header-btn-lg pr-0">
+          <div className="widget-content p-0">
+            <div className="widget-content-wrapper">
+              {/* <div className="widget-content-left">
                                 <UncontrolledButtonDropdown>
                                     <DropdownToggle color="link" className="p-0">
                                         <img width={42} className="rounded-circle" src={avatar1} alt=""/>
@@ -166,31 +143,37 @@ class UserBox extends React.Component {
                                     </DropdownMenu>
                                 </UncontrolledButtonDropdown>
                             </div> */}
-                            <div className="widget-content-left  ml-3 header-user-info">
-                                <div className="widget-heading">
-                                    Welcome
-                                </div>
-                                <div className="widget-subheading">
-                                    {getShop()}
-                                </div>
-                            </div>
+              <div className="widget-content-left  ml-3 header-user-info">
+                <div className="widget-heading">Welcome</div>
+                <div className="widget-subheading">{getShop()}</div>
+              </div>
 
-                            <div className="widget-content-right header-user-info ml-3">
-                                <Button className="btn-shadow p-1" size="sm" onClick={()=> {window.open('mailto:support@appit.app')}} color="info"
-                                        id="Tooltip-1">
-                                        <i className="pe-7s-headphones btn-icon-wrapper mb-2" fontSize="20px"/>
-                                    {/* <Ionicon color="#ffffff" fontSize="20px" icon="pe-7s-headphones"/> */}
-                                </Button>
-                                {/* <UncontrolledTooltip placement="bottom" target={'Tooltip-1'}>
+              <div className="widget-content-right header-user-info ml-3">
+                <Button
+                  className="btn-shadow p-1"
+                  size="sm"
+                  onClick={() => {
+                    window.open("mailto:support@appit.app");
+                  }}
+                  color="info"
+                  id="Tooltip-1"
+                >
+                  <i
+                    className="pe-7s-headphones btn-icon-wrapper mb-2"
+                    fontSize="20px"
+                  />
+                  {/* <Ionicon color="#ffffff" fontSize="20px" icon="pe-7s-headphones"/> */}
+                </Button>
+                {/* <UncontrolledTooltip placement="bottom" target={'Tooltip-1'}>
                                     Click for Toastify Notifications!
                                 </UncontrolledTooltip> */}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </Fragment>
-        )
-    }
+              </div>
+            </div>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default UserBox;
