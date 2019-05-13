42e9aa59c970a83040ed2ea7df1588425f2aa835
Rishabh Tayal <rtayal11@gmail.com>
2019-05-13 15:09:25 -0500

    Update index.js

--- architectui-react-pro/src/DemoPages/Dashboards/PreviewApp/index.js
+++ architectui-react-pro/src/DemoPages/Dashboards/PreviewApp/index.js
@@ -1,17 +1,19 @@
 import { Snackbar } from "@material-ui/core";
-import axios from 'axios';
+import axios from "axios";
 import React, { Fragment } from "react";
 import ReactCSSTransitionGroup from "react-addons-css-transition-group";
-import { Button } from "reactstrap";
-import { API_ROOT } from "../../../utilities/api-config";
+import LaddaButton, { EXPAND_RIGHT } from "react-ladda";
+import { Button, Card, CardBody, Col, Row } from "reactstrap";
 import PageTitle from "../../../Layout/AppMain/PageTitle";
+import { API_ROOT } from "../../../utilities/api-config";
 
 export default class PreviewApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       loading: false,
-      snackbarOpen: false
+      snackbarOpen: false,
+      expRight: false
     };
   }
 
@@ -23,11 +25,20 @@ export default class PreviewApp extends React.Component {
         const data = response.data;
         this.setState({ loading: false });
         this.setState({ snackbarOpen: true });
+        this.toggle("expRight");
       })
       .catch(error => {
         this.setState({ loading: false });
+        this.toggle("expRight");
       });
   };
+
+  toggle(name) {
+    this.setState({
+      [name]: !this.state[name],
+      progress: 0.5
+    });
+  }
 
   render() {
     return (
@@ -45,9 +56,32 @@ export default class PreviewApp extends React.Component {
             subheading="This is an example dashboard created using build-in elements and components."
             icon="pe-7s-phone icon-gradient bg-mean-fruit"
           />
-          <p>Your app is ready to preview on your device.</p>
-          <p>Download the AppIt preview app on your device.</p>
-          <Button onClick={this.handleButtonClick}>Get on Email</Button>
+          <Card className="main-card mb-3">
+            <CardBody>
+              <div className="text-center">
+                <h2>Your app is ready to preview!</h2>
+                <h4>Download the AppIt preview app on your device.</h4>
+
+                <LaddaButton
+                  className="mb-2 mr-2 btn btn-shadow btn-primary"
+                  loading={this.state.expRight}
+                  onClick={() => {
+                    this.toggle("expRight");
+                    this.handleButtonClick();
+                  }}
+                  data-style={EXPAND_RIGHT}
+                >
+                  Get on Email
+                </LaddaButton>
+
+                {/* <div>
+                  <Button onClick={()=> window.open('./dashboards/design-landing')}>Customize</Button>
+                  {' '}
+                  <Button>Go Live</Button>
+                </div> */}
+              </div>
+            </CardBody>
+          </Card>
           <Snackbar
             style={{ marginBottom: "10px" }}
             open={this.state.snackbarOpen}
