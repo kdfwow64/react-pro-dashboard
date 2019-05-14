143b77e8727483651e9567ad7654b29972ed44c9
Rishabh Tayal <rtayal11@gmail.com>
2019-05-14 11:29:48 -0500

    formatting

--- architectui-react-pro/src/DemoPages/Dashboards/Publish/Steps/Step1.js
+++ architectui-react-pro/src/DemoPages/Dashboards/Publish/Steps/Step1.js
@@ -1,55 +1,75 @@
-import React, { Fragment } from 'react';
-import { Col, FormGroup, Input, Label, Row } from 'reactstrap';
-import S3SingleFileUploaderWithPreviewAndFileNameCapability from '../../../../utilities/S3SingleFileUploaderWithPreviewAndFileNameCapability';
+import React, { Fragment } from "react";
+import { FormGroup, Input, Label } from "reactstrap";
+import S3SingleFileUploaderWithPreviewAndFileNameCapability from "../../../../utilities/S3SingleFileUploaderWithPreviewAndFileNameCapability";
 
 export default class WizardStep1 extends React.Component {
-    render() {
-        return (
-            <Fragment>
-                <div className="form-wizard-content">
-                    {/* <Row form> */}
-                        {/* <Col md={6}> */}
-                            <FormGroup>
-                                <Label for="exampleEmail55">App Name</Label>
-                                <Input type="text" name="app-name" id="exampleEmail55" placeholder="Your app name"/>
-                            </FormGroup>
-                        {/* </Col> */}
-                        {/* <Col md={6}>
-                            <FormGroup>
-                                <Label for="examplePassword22">Password</Label>
-                                <Input type="password" name="password" id="examplePassword22"
-                                       placeholder="password placeholder"/>
-                            </FormGroup>
-                        </Col> */}
-                    {/* </Row> */}
-                    <FormGroup>
-                        <Label for="exampleAddress">Short description of the app</Label>
-                        <Input type="text" name="address" id="exampleAddress" placeholder=""/>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label for="exampleAddress2">Long description of the app</Label>
-                        <Input type="textarea" name="address2" id="exampleAddress2"
-                               placeholder=""/>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label>App Icon</Label>
-                        <Input type="file" />
-                    </FormGroup>
+  render() {
+    return (
+      <Fragment>
+        <div className="form-wizard-content">
+          <FormGroup>
+            <Label for="exampleEmail55">App Name</Label>
+            <Input
+              type="text"
+              name="app-name"
+              id="exampleEmail55"
+              placeholder="Your app name"
+            />
+          </FormGroup>
 
-                    <FormGroup>
-                        <Label>Splash Screen</Label>
-                        <S3SingleFileUploaderWithPreviewAndFileNameCapability
-                          label="Add Splash Screen"
-                          acceptedFiles={["image/jpeg", "image/png"]}
-                        //   fileName='splash_screen'
-                          previewImageHeight={"150px"}
-                          previewImageWidth={"100px"}
-                          imageFolder={"splash_screen"}
-                          onChange={()=>{}}
-                        />
-                        {/* <Input type="file" /> */}
-                    </FormGroup>
-                    {/* <Row form>
+          <FormGroup>
+            <Label for="exampleAddress">Short description of the app</Label>
+            <Input
+              type="text"
+              name="address"
+              id="exampleAddress"
+              placeholder=""
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="exampleAddress2">Long description of the app</Label>
+            <Input
+              type="textarea"
+              name="address2"
+              id="exampleAddress2"
+              placeholder=""
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="keywords">Keywords</Label>
+            <Input type="text" name="keywords" id="keywords" placeholder="" />
+          </FormGroup>
+
+          <FormGroup>
+            <Label>App Icon</Label>
+            <S3SingleFileUploaderWithPreviewAndFileNameCapability
+              label="Add App Icon"
+              acceptedFiles={["image/jpeg", "image/png"]}
+              //   fileName='splash_screen'
+              previewImageHeight={"100px"}
+              previewImageWidth={"100px"}
+              imageFolder={"app_icon"}
+              onChange={() => {}}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <Label>Splash Screen</Label>
+            <S3SingleFileUploaderWithPreviewAndFileNameCapability
+              label="Add Splash Screen"
+              acceptedFiles={["image/jpeg", "image/png"]}
+              //   fileName='splash_screen'
+              previewImageHeight={"150px"}
+              previewImageWidth={"100px"}
+              imageFolder={"splash_screen"}
+              onChange={() => {}}
+            />
+            {/* <Input type="file" /> */}
+          </FormGroup>
+
+          {/* <Row form>
                         <Col md={6}>
                             <FormGroup>
                                 <Label for="exampleCity">City</Label>
@@ -69,12 +89,12 @@ export default class WizardStep1 extends React.Component {
                             </FormGroup>
                         </Col>
                     </Row> */}
-                    {/* <FormGroup check>
+          {/* <FormGroup check>
                         <Input type="checkbox" name="check" id="exampleCheck"/>
                         <Label for="exampleCheck" check>Check me out</Label>
                     </FormGroup> */}
-                </div>
-            </Fragment>
-        );
-    }
+        </div>
+      </Fragment>
+    );
+  }
 }
