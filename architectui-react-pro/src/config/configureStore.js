2c3d49c0eb0ec278c2e213877fd7ab2f88b63c7a
Rishabh Tayal <rtayal11@gmail.com>
2019-05-15 23:06:25 -0500

    Update index.js

--- architectui-react-pro/src/config/configureStore.js
+++ architectui-react-pro/src/config/configureStore.js
@@ -7,14 +7,15 @@ import authentication from '../utilities/authentication';
 // import appMiscSettings from '../utilities/app-misc-settings';
 import appSettings from '../utilities/app-settings';
 // import collectionListings from '../utilities/collection-listing';
-import goLive from '../utilities/go-live';
+import goLive from '../reducers/go-live';
 // import providerShopDetails from '../utilities/provider-shop-details';
 import triggerBuild from '../utilities/trigger-build';
+import ThemeOptions from '../reducers/ThemeOptions';
 
 export default function configureStore() {
-  return createStore(
+  // const store = createStore(reducer, initialState, composedMiddlewares(middlewares));
+  const store = createStore(
     combineReducers({
-      ...reducers,
       authentication,
       goLive,
       appSettings,
@@ -24,8 +25,18 @@ export default function configureStore() {
       triggerBuild,
       // providerShopDetails,
       // appHomePage
-      form: reduxFormReducer
+      // form: reduxFormReducer,
+      // ...reducers
+      ThemeOptions
     }),
-    {},
+    {}
   );
+  if (module.hot) {
+    // TODO : see if reducers can be moved to feature modules and still get HMR working
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers').default;
+      store.replaceReducer(nextReducer);
+    });
+  }
+  return store;
 }
