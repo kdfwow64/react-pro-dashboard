e6fd1c66694ac0d1a4beb654a8a55bf622011cd8
Rishabh Tayal <rtayal11@gmail.com>
2019-05-17 11:53:19 -0500

    menu fix

--- architectui-react-pro/src/Layout/AppNav/VerticalNavWrapper.js
+++ architectui-react-pro/src/Layout/AppNav/VerticalNavWrapper.js
@@ -22,14 +22,14 @@ class Nav extends Component {
                 <MetisMenu content={ComponentsNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/> */}
         {/* <h5 className="app-sidebar__heading">Dashboard Widgets</h5>
                 <MetisMenu content={WidgetsNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/> */}
-        <h5 className="app-sidebar__heading">Forms</h5>
-        <MetisMenu
+        {/* <h5 className="app-sidebar__heading">Forms</h5> */}
+        {/* <MetisMenu
           content={FormsNav}
           activeLinkFromLocation
           className="vertical-nav-menu"
           iconNamePrefix=""
           classNameStateIcon="pe-7s-angle-down"
-        />
+        /> */}
         {/* <h5 className="app-sidebar__heading">Charts</h5>
                 <MetisMenu content={ChartsNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/> */}
       </Fragment>
