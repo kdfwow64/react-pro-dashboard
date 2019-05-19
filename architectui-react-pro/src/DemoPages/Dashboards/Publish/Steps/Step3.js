7268c6fa78a764cce1d2a78bd384bbdd96794863
Rishabh Tayal <rtayal11@gmail.com>
2019-05-18 23:26:23 -0500

    Update Step3.js

--- architectui-react-pro/src/DemoPages/Dashboards/Publish/Steps/Step3.js
+++ architectui-react-pro/src/DemoPages/Dashboards/Publish/Steps/Step3.js
@@ -2,6 +2,28 @@ import React, { Fragment } from 'react';
 import { Button } from 'reactstrap';
 
 export default class WizardStep3 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggleApple = this.toggleApple.bind(this);
+    this.toggleGoogle = this.toggleGoogle.bind(this);
+    this.state = {
+      appleSelected: false,
+      googleSelected: false
+    };
+  }
+
+  toggleApple() {
+    this.setState(prevState => ({
+      appleSelected: !prevState.appleSelected
+    }));
+  }
+
+  toggleGoogle() {
+    this.setState(prevState => ({
+      googleSelected: !prevState.googleSelected
+    }));
+  }
+
   render() {
     return (
       <Fragment>
@@ -15,17 +37,23 @@ export default class WizardStep3 extends React.Component {
             <div className="text-center">
               <Button
                 color="success"
-                type="submit"
+                // type="submit"
+                outline
                 size="lg"
+                active={this.state.appleSelected}
                 className="btn-shadow btn-wide mr-3"
+                onClick={this.toggleApple}
               >
                 Apple
               </Button>
               <Button
                 color="success"
-                type="submit"
+                // type="submit"
+                outline
                 size="lg"
+                active={this.state.googleSelected}
                 className="btn-shadow btn-wide"
+                onClick={this.toggleGoogle}
               >
                 Google
               </Button>
