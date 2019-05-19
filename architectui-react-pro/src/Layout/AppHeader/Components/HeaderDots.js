317efcf73d7b6354d771b5d33f2b74435e4cfd1a
Rishabh Tayal <rtayal11@gmail.com>
2019-05-19 01:08:30 -0500

    what's new badge

--- architectui-react-pro/src/Layout/AppHeader/Components/HeaderDots.js
+++ architectui-react-pro/src/Layout/AppHeader/Components/HeaderDots.js
@@ -1,7 +1,14 @@
 import React, { Fragment } from 'react';
 import Ionicon from 'react-ionicons';
 import Tabs from 'react-responsive-tabs';
-import { Button, DropdownMenu, DropdownToggle, Nav, NavItem, UncontrolledDropdown } from 'reactstrap';
+import {
+  Button,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  NavItem,
+  UncontrolledDropdown
+} from 'reactstrap';
 import city3 from '../../../assets/utils/images/dropdown-header/city3.jpg';
 // Dropdown Tabs Content
 import ChatExample from './TabsContent/ChatExample';
@@ -9,57 +16,56 @@ import SysErrEx from './TabsContent/SystemExample';
 import TimelineEx from './TabsContent/TimelineExample';
 
 const data = [
-    {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-    {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-    {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-    {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-    {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-    {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-    {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
-    {name: 'Page C', uv: 2000, pv: 6800, amt: 2290},
-    {name: 'Page D', uv: 4780, pv: 7908, amt: 2000},
-    {name: 'Page E', uv: 2890, pv: 9800, amt: 2181},
-    {name: 'Page F', uv: 1390, pv: 3800, amt: 1500},
-    {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+  { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
+  { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
+  { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
+  { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
+  { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
+  { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
+  { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
+  { name: 'Page C', uv: 2000, pv: 6800, amt: 2290 },
+  { name: 'Page D', uv: 4780, pv: 7908, amt: 2000 },
+  { name: 'Page E', uv: 2890, pv: 9800, amt: 2181 },
+  { name: 'Page F', uv: 1390, pv: 3800, amt: 1500 },
+  { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 }
 ];
 
 const tabsContent = [
-    {
-        title: 'Messages',
-        content: <ChatExample/>
-    },
-    {
-        title: 'Events',
-        content: <TimelineEx/>
-    },
-    {
-        title: 'System Errors',
-        content: <SysErrEx/>
-    },
+  {
+    title: 'Messages',
+    content: <ChatExample />
+  },
+  {
+    title: 'Events',
+    content: <TimelineEx />
+  },
+  {
+    title: 'System Errors',
+    content: <SysErrEx />
+  }
 ];
 
 function getTabs() {
-    return tabsContent.map((tab, index) => ({
-        title: tab.title,
-        getContent: () => tab.content,
-        key: index,
-    }));
+  return tabsContent.map((tab, index) => ({
+    title: tab.title,
+    getContent: () => tab.content,
+    key: index
+  }));
 }
 
 class HeaderDots extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            active: false,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: false
+    };
+  }
 
-    }
-
-    render() {
-        return (
-            <Fragment>
-                <div className="header-dots">
-                    {/* <UncontrolledDropdown>
+  render() {
+    return (
+      <Fragment>
+        <div className="header-dots">
+          {/* <UncontrolledDropdown>
                         <DropdownToggle className="p-0 mr-2" color="link">
                             <div className="icon-wrapper icon-wrapper-alt rounded-circle">
                                 <div className="icon-wrapper-bg bg-primary"/>
@@ -142,20 +148,30 @@ class HeaderDots extends React.Component {
                             </Nav>
                         </DropdownMenu>
                     </UncontrolledDropdown> */}
-                    <UncontrolledDropdown>
-                        <DropdownToggle className="p-0 mr-2" color="link">
-                            <div className="icon-wrapper icon-wrapper-alt rounded-circle">
-                                <div className="icon-wrapper-bg bg-danger"/>
-                                <Ionicon beat={true} color="#d92550" fontSize="23px" icon="md-notifications-outline"/>
-                                <div className="badge badge-dot badge-dot-sm badge-danger">Notifications</div>
-                            </div>
-                        </DropdownToggle>
-                        <DropdownMenu right className="dropdown-menu-xl rm-pointers">
+          <UncontrolledDropdown>
+            <DropdownToggle className="p-0 mr-2" color="link">
+              <div
+                id="beamerButton"
+                className="icon-wrapper icon-wrapper-alt rounded-circle"
+              >
+                <div className="icon-wrapper-bg bg-danger" />
+                <Ionicon
+                  beat
+                  color="#d92550"
+                  fontSize="23px"
+                  icon="md-notifications-outline"
+                />
+                <div className="badge-dot badge-dot-sm badge-danger">
+                  Notifications
+                </div>
+              </div>
+            </DropdownToggle>
+            {/* <DropdownMenu right className="dropdown-menu-xl rm-pointers">
                             <div className="dropdown-menu-header mb-0">
                                 <div className="dropdown-menu-header-inner bg-deep-blue">
                                     <div className="menu-header-image opacity-1"
                                          style={{
-                                             backgroundImage: 'url(' + city3 + ')'
+                                             backgroundImage: `url(${  city3  })`
                                          }}
                                     />
                                     <div className="menu-header-content text-dark">
@@ -164,7 +180,7 @@ class HeaderDots extends React.Component {
                                     </div>
                                 </div>
                             </div>
-                            <Tabs tabsWrapperClass="body-tabs body-tabs-alt" transform={false} showInkBar={true}
+                            <Tabs tabsWrapperClass="body-tabs body-tabs-alt" transform={false} showInkBar
                                   items={getTabs()}/>
                             <Nav vertical>
                                 <NavItem className="nav-item-divider"/>
@@ -174,9 +190,9 @@ class HeaderDots extends React.Component {
                                     </Button>
                                 </NavItem>
                             </Nav>
-                        </DropdownMenu>
-                    </UncontrolledDropdown>
-                    {/* <UncontrolledDropdown>
+                        </DropdownMenu> */}
+          </UncontrolledDropdown>
+          {/* <UncontrolledDropdown>
                         <DropdownToggle className="p-0 mr-2" color="link">
                             <div className="icon-wrapper icon-wrapper-alt rounded-circle">
                                 <div className="icon-wrapper-bg bg-focus"/>
@@ -227,7 +243,7 @@ class HeaderDots extends React.Component {
                             </DropdownItem>
                         </DropdownMenu>
                     </UncontrolledDropdown> */}
-                    {/* <UncontrolledDropdown>
+          {/* <UncontrolledDropdown>
                         <DropdownToggle className="p-0" color="link">
                             <div className="icon-wrapper icon-wrapper-alt rounded-circle">
                                 <div className="icon-wrapper-bg bg-success"/>
@@ -308,10 +324,10 @@ class HeaderDots extends React.Component {
                             </Nav>
                         </DropdownMenu>
                     </UncontrolledDropdown> */}
-                </div>
-            </Fragment>
-        )
-    }
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default HeaderDots;
--- architectui-react-pro/src/Layout/AppHeader/index.js
+++ architectui-react-pro/src/Layout/AppHeader/index.js
@@ -3,57 +3,63 @@ import React, { Fragment } from 'react';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { connect } from 'react-redux';
 import HeaderLogo from '../AppLogo';
-import HeaderDots from "./Components/HeaderDots";
-import HeaderRightDrawer from "./Components/HeaderRightDrawer";
+import HeaderDots from './Components/HeaderDots';
+import HeaderRightDrawer from './Components/HeaderRightDrawer';
 import UserBox from './Components/UserBox';
 
 class Header extends React.Component {
-    render() {
-        let {
-            headerBackgroundColor,
-            enableMobileMenuSmall,
-            enableHeaderShadow
-        } = this.props;
-        return (
-            <Fragment>
-                <ReactCSSTransitionGroup
-                    component="div"
-                    className={cx("app-header", headerBackgroundColor, {'header-shadow': enableHeaderShadow})}
-                    transitionName="HeaderAnimation"
-                    transitionAppear={true}
-                    transitionAppearTimeout={1500}
-                    transitionEnter={false}
-                    transitionLeave={false}>
+  render() {
+    let {
+      headerBackgroundColor,
+      enableMobileMenuSmall,
+      enableHeaderShadow
+    } = this.props;
+    return (
+      <Fragment>
+        <ReactCSSTransitionGroup
+          component="div"
+          className={cx('app-header', headerBackgroundColor, {
+            'header-shadow': enableHeaderShadow
+          })}
+          transitionName="HeaderAnimation"
+          transitionAppear
+          transitionAppearTimeout={1500}
+          transitionEnter={false}
+          transitionLeave={false}
+        >
+          <HeaderLogo />
 
-                    <HeaderLogo/>
-
-                    <div className={cx(
-                        "app-header__content",
-                        {'header-mobile-open': enableMobileMenuSmall},
-                    )}>
-                        {/* <div className="app-header-left">
+          <div
+            className={cx('app-header__content', {
+              'header-mobile-open': enableMobileMenuSmall
+            })}
+          >
+            {/* <div className="app-header-left">
                             <SearchBox/>
                             <MegaMenu/>
                         </div> */}
-                        <div className="app-header-right">
-                            {/* <HeaderDots/> */}
-                            <UserBox/>
-                            {/* <HeaderRightDrawer/> */}
-                        </div>
-                    </div>
-                </ReactCSSTransitionGroup>
-            </Fragment>
-        );
-    }
+            <div className="app-header-right">
+              <HeaderDots />
+              <UserBox />
+              {/* <HeaderRightDrawer/> */}
+            </div>
+          </div>
+        </ReactCSSTransitionGroup>
+      </Fragment>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    enableHeaderShadow: state.ThemeOptions.enableHeaderShadow,
-    closedSmallerSidebar: state.ThemeOptions.closedSmallerSidebar,
-    headerBackgroundColor: state.ThemeOptions.headerBackgroundColor,
-    enableMobileMenuSmall: state.ThemeOptions.enableMobileMenuSmall,
+  enableHeaderShadow: state.ThemeOptions.enableHeaderShadow,
+  closedSmallerSidebar: state.ThemeOptions.closedSmallerSidebar,
+  headerBackgroundColor: state.ThemeOptions.headerBackgroundColor,
+  enableMobileMenuSmall: state.ThemeOptions.enableMobileMenuSmall
 });
 
 const mapDispatchToProps = dispatch => ({});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
